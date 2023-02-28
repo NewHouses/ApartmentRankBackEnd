13ac83512e4d1c18db25ee2380af15c4f98dad34
NewHouses <sergio.simons.c@gmail.com>
2023-02-28 20:08:05 +0100

    Adapting IdealistaResponse to ApartmentRankResponse

---
+++ Domain/ApartmentRank.Domain/Entities/ApartmentRankResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ApartmentRank.Domain.Entities
+{
+    public class ApartmentRankResponse
+    {
+        public IEnumerable<Apartment> apartments { get; set; }
+        public int total { get; set; }
+
+        public ApartmentRankResponse(IEnumerable<Apartment> apartments, int total) 
+        {
+            this.apartments = apartments;
+            this.total = total;
+        }
+    }
+}
---
+++ Domain/ApartmentRank.Domain/ValueObjects/Idealista/IdealistaResponseAdapter.cs
@@ -0,0 +1,29 @@
+﻿using ApartmentRank.Domain.Entities;
+using ApartmentRank.Domain.Entities.Idealista;
+
+namespace ApartmentRank.Domain.ValueObjects.Idealista
+{
+    public class IdealistaResponseAdapter
+    {
+        public readonly IdealistaResponse idealistaResponse;
+
+        public IdealistaResponseAdapter(IdealistaResponse idealistaResponse)
+        {
+            this.idealistaResponse = idealistaResponse;
+        }
+
+        public ApartmentRankResponse Convert()
+        {
+            var apartments = idealistaResponse.elementList.Select(ap =>
+            {
+                var name = ap.suggestedTexts?.title + " " + ap.suggestedTexts?.subtitle;
+                var apartment = new Apartment(name, ap.description, ap.thumbnail, ap.price, ap.propertyType, ap.operation, ap.size, ap.exterior, ap.rooms, ap.bathrooms, ap.latitude, ap.longitude, ap.url, ap.status, ap.newDevelopment, ap.parkingSpace);
+                apartment.apartmentAttributes = new [] { new ApartmentAttribute("hasLift", ap.hasLift) };
+                return apartment;
+            }
+            );
+           
+            return new ApartmentRankResponse(apartments, idealistaResponse.total);
+        }
+    }
+}
